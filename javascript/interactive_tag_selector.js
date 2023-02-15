ebc3bc126415015f260350a7d75ea7b6aff09451
blue.pen5805 <blue.pen5805@fastmail.com>
2023-02-15 16:49:11 +0900

    almost-completed

---
+++ javascript/interactive_tag_selector.js
@@ -0,0 +1,154 @@
+let visible = false
+const areaId = 'interactive-tag-selector'
+const selectId = 'interactive-tag-selector-select'
+const contentId = 'interactive-tag-selector-content'
+
+async function readFile(filepath) {
+  const response = await fetch(`file=${filepath}`);
+
+  return await response.text();
+}
+
+function createTagButtons(tags, prefix = '') {
+  if(Array.isArray(tags)) {
+    return tags.map((tag) => createTagButton(tag, tag, 'secondary'))
+  } else {
+    return Object.keys(tags).map((key) => {
+      const values = tags[key]
+      const randomKey = `${prefix}:${key}`
+
+      if (typeof values === 'string') { return createTagButton(key, values, 'secondary') }
+
+      const group = document.createElement('div')
+      group.classList.add('gr-block', 'gr-box', 'relative', 'w-full', 'border-solid', 'border', 'border-gray-200', 'flex', 'flex-col', 'col', 'flex-wrap', 'gap-2', 'p-2')
+      group.style = `min-width: min(320px, 100%); flex-basis: 50%; flex-grow: 1;`
+      group.append(createTagButton(key, `@${randomKey}@`))
+      group.insertAdjacentHTML('beforeend', '<div class="flex flex-col buttons"></div>')
+
+      const buttons = group.querySelector('.buttons')
+      buttons.classList.add('gr-block', 'gr-box', 'relative', 'w-full', 'flex', 'flex-wrap')
+      buttons.style = 'flex-direction: initial;'
+
+      createTagButtons(values, randomKey).forEach((button) => {
+        buttons.appendChild(button)
+      })
+
+      return group
+    })
+  }
+}
+
+function createTagButton(title, value, color = 'primary') {
+  const button = document.createElement('button')
+  button.classList.add('gr-button', 'gr-button-sm', `gr-button-${color}`)
+  button.style = 'height: 2rem; flex-grow: 0; margin: 2px;'
+  button.textContent = title
+  button.addEventListener('click', () => { addTag(value) })
+
+  return button
+}
+
+function createTagArea(tags = {}) {
+  const tagArea = document.createElement('div')
+  tagArea.id = areaId
+  tagArea.classList.add('flex', 'flex-col', 'relative', 'col', 'gr-panel')
+  tagArea.style = 'display: none;'
+
+  tagArea.innerHTML = `
+    <div class="flex flex-col relative col">
+      <div class="gr-block gr-box relative w-full border-solid border border-gray-200">
+        <select id="${selectId}" class="gr-box gr-input w-full">
+          <option>なし</option>
+        </select>
+        <div id="${contentId}" class="flex flex-row flex-wrap"></div>
+      </div>
+    </div>
+  `
+  const select = tagArea.querySelector(`#${selectId}`)
+  const content = tagArea.querySelector(`#${contentId}`)
+
+  Object.keys(tags).forEach((key) => {
+    const values = tags[key]
+
+    const option = document.createElement('option')
+    option.value = key
+    option.textContent = key
+    select.appendChild(option)
+
+    const container = document.createElement('div')
+    container.id = `interactive-tag-selector-container-${key}`
+    container.classList.add('flex', 'flex-row', 'flex-wrap')
+    container.style = 'display: none;'
+
+    createTagButtons(values, key).forEach((group) => {
+      container.appendChild(group)
+    })
+
+    content.appendChild(container)
+  })
+
+  select.addEventListener('change', (event) => {
+    const selected = event.target.value
+    Array.from(content.childNodes).forEach((node) => {
+      const visible = node.id === `interactive-tag-selector-container-${selected}`
+      changeVisibility(node, visible)
+    })
+  })
+
+
+  gradioApp().getElementById('txt2img_toprow').after(tagArea)
+}
+
+function changeVisibility(node, visible) {
+  style = visible ? 'display: flex;' : 'display: none;'
+  node.style = style
+}
+
+function addTag(tag) {
+  const textarea = gradioApp().getElementById('txt2img_prompt').querySelector('textarea')
+
+  if (textarea.value.trim() !== '' && textarea.value.trim().slice(-1) !== ',') { textarea.value += ', '}
+  textarea.value += tag
+
+  updateInput(textarea)
+}
+
+const PATH_FILE = 'tmp/interactiveTagSelector.txt'
+async function parseFiles() {
+  yaml = window.jsyaml
+
+  const text = await readFile(PATH_FILE);
+  if (text === '') { return {} }
+
+  const paths = text.split(/\r\n|\n/)
+
+  tags = {}
+  for (const path of paths) {
+    const filename = path.split('/').pop().split('.').shift()
+    const data = await readFile(path)
+    yaml.loadAll(data, function (doc) {
+      tags[filename] = doc
+    })
+  }
+
+  return tags
+}
+
+onUiLoaded(async () => {
+  const tags = await parseFiles()
+
+  const button = document.createElement('button')
+  button.textContent = '🔯タグを選択'
+  button.classList.add('gr-button', 'gr-button-sm', 'gr-button-secondary')
+  button.style = 'margin-top: 0.5rem;'
+
+  button.addEventListener('click', () => {
+    const tagArea = gradioApp().querySelector(`#${areaId}`)
+    changeVisibility(tagArea, visible = !visible)
+  })
+
+  const txt2imgActionColumn = gradioApp().getElementById('txt2img_actions_column')
+  txt2imgActionColumn.appendChild(button)
+
+  createTagArea(tags)
+})
---
+++ scripts/interactive_tag_selector.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+import random
+import re
+import yaml
+
+from modules.scripts import Script, AlwaysVisible, basedir
+
+FILE_DIR = Path().absolute()
+BASE_DIR = Path(basedir())
+TAGS_DIR = BASE_DIR.joinpath('tags')
+
+def tag_files():
+    return TAGS_DIR.rglob("*.yml")
+
+def load_tags():
+    tags = {}
+    for filepath in tag_files():
+        with open(filepath, "r", encoding="utf-8") as file:
+            yml = yaml.safe_load(file)
+            tags[filepath.stem] = yml
+
+    return tags
+
+def find_tag(tags, location):
+    if type(location) == str:
+        return tags[location]
+
+    value = ''
+    if len(location) > 0:
+        value = tags
+        for tag in location:
+            value = value[tag]
+
+    if (type(value) == list):
+        value = random.choice(value)
+    elif (type(value) == dict):
+        key = random.choice(list(value.keys()))
+        tag = value[key]
+        if type(tag) == dict:
+            value = find_tag(tag, random.choice(list(tag.keys())))
+        else:
+            value = find_tag(value, key)
+
+    return value
+
+class Script(Script):
+    tags = {}
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.tags = load_tags()
+
+    def title(self) -> str:
+        return "Interactive Tag Selector"
+
+    def show(self, is_img2img)-> bool|object:
+        return AlwaysVisible
+
+    def ui(self, is_img2img):
+        return None
+
+    def process(self, p):
+        # Replace template tags
+        prompt = p.all_prompts[0]
+
+        for match in re.finditer(r'(@([^>]+?)@)', p.prompt):
+            template = match.group(0)
+            value = find_tag(self.tags, template[1:-1].split(':'))
+            prompt = prompt.replace(template, value, 1)
+
+        p.prompt = prompt
+        for i in range(len(p.all_prompts)):
+            p.all_prompts[i] = prompt
